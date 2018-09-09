0a4649aae3db948a1f7278d3428c12f35495347d
Mathew Hall <mhall.js@gmail.com>
2018-09-09 20:21:37 +0100

    LibraryList component added

---
+++ src/components/LibraryList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+  render() {
+    return(
+      <View />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    libraries: state.libraries
+  }
+}
+
+export default connect(mapStateToProps)(LibraryList);
